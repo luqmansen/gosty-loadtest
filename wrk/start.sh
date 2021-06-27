df90d8c74c7b934890ec54d9a46450f6d3ebebce
luqmansen <luqmansen@gmail.com>
2021-06-27 11:35:38 +0700

    add latency options to script

--- wrk/start.sh
+++ wrk/start.sh
@@ -5,7 +5,7 @@ CONN=$1
 echo using $CONN connection to $URL
 
 startdate=$(date)
-sudo docker run --rm -v `pwd`/script:/scripts -v `pwd`/data:/data czerasz/wrk-json wrk -c$CONN -t4 -d300s -s /scripts/req.lua $URL
+sudo docker run --rm -v `pwd`/script:/scripts -v `pwd`/data:/data czerasz/wrk-json wrk -c$CONN -t4 -d300s --latency -s /scripts/req.lua $URL
 enddate=$(date)
 echo $startdate
 echo $enddate
